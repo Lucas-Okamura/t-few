a2a53f4b6269c7bc245f361f8fcbdeb59b528e9c
Lucas Okamura <lucasokamura@gmail.com>
2023-09-28 20:26:46 -0300

    minor change in sh file

--- bin/few-shot-pretrained-3b-100k-news.sh
+++ bin/few-shot-pretrained-3b-100k-news.sh
@@ -1,3 +1,3 @@
-python -m src.pl_train -c t03b.json+ia3.json+news_articles_ptbr.json \
+python -m src.pl_train -c t03b.json+ia3.json+news-articles-ptbr.json \
     -k load_weight="pretrained_checkpoints/t03b_ia3_finish.pt" exp_name=t03b_folha_seed_42_ia3_pretrained100k few_shot_random_seed=42 seed=42\
     test_size=50000 num_shot=64 few_shot=False num_steps=304
